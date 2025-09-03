7672c02cdf23e5fc152c78c949415122e029a14f
SandyT123 <sandeeptaradale79@gmail.com>
2025-09-03 13:56:34 +0530

    Datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 1414141;
+let accountEmail = "sandy@123.com";
+var accountPassword = 12345;
+accountCity = "jaipur";
+let accountState;
+
+// accountId = 2 not allowed
+accountEmail = "sdfghjk@jj.com";
+accountPassword = "212121"
+accountCity = "banglore";
+
+// console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity, accountState])
+
+
+//prefer not use to var becz of issue in block scope in functional scopess 
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // Treat all js code as newer version
+
+// alert("Hello") we are using nodejs, not browser
+
+console.log(3+
+    3); //code readablity should be high
+console.log("heello");
+
+//tc 39.es ecmasript doc
+//mdn doc
+
+let name ="sandeep";
+let age =23;
+let isLoggedIn = true;
+
+//please visit above docs for this
+//premitive datatypes
+//number => 2 to the power of 53
+//bigInt 
+//string => "sandeep"
+//boolean=> true or false
+//null => standalone value or empty value
+//undefiened 
+//symbol => uniqueness
+
+console.log(typeof "sandeep");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
+

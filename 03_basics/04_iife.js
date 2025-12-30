cf9965aa4fe78636a99d4d5b67b3f061878b43f8
SandyT123 <sandeeptaradale79@gmail.com>
2025-12-30 14:18:37 +0530

    added updated data

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -11,3 +11,22 @@
 // Call stack in js
 // Order of exicution through exicution context ===> lifo -last in first out
 // Exicution context for global and functional EC
+const user = {
+  husbandAddress: "Taradale",
+  wifeAdresss: "patil",
+};
+const userSpouse = {
+  husband: "sandeep",
+  wife: "sandhya",
+};
+
+function welcomeMessage(date, place) {
+  console.log(
+    `${this.husband} ${user.husbandAddress}, will marry ${this.wife} ${user.wifeAdresss} soon on ${date} and place is ${place}`
+  );
+}
+welcomeMessage.call(userSpouse, "march 26 2028", "ankali");
+welcomeMessage.apply(userSpouse, ["march 26 2028", "ankali"]);
+let box = welcomeMessage.bind(userSpouse, "march 26 2028", "ankali");
+// console.log(box);
+box();
